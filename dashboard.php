60715aeaa82f93aae9a5e7fb7c02cc40039e3742
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2023-12-17 22:19:15 +0300

    setting up first things

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome To The Dashboard</title>
+    </head>
+    <body>
+
+
+        <div id="signalert" style="text-align: center;border:1px solid #000;width:200px;padding:40px;margin:auto">
+            <p>Please <a href="method.php" style="text-decoration:none">Sign In</a> To Continue</p>
+        </div>
+
+
+    </body>
+</html>
