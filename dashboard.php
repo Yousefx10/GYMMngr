57977f179a938ba42dcc7527b0ab7e9e9cd24891
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2023-12-18 01:19:11 +0300

    sign in message shows when not signed in

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
-echo  $_SESSION['user_id'];
+$signed=false;
+
+if(isset($_SESSION['user_id']))
+{
+    if($_SESSION['user_id']!="0")  {$signed=true;}
+}
+
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,10 +17,14 @@ echo  $_SESSION['user_id'];
     </head>
     <body>
 
+<?php
+$htmlform1='<div id="signalert" style="text-align: center;border:1px solid #000;width:200px;padding:40px;margin:auto">
+<p>Please <a href="method.php" style="text-decoration:none">Sign In</a> To Continue</p>
+</div>';
+echo $signed ? "" : $htmlform1;
 
-        <div id="signalert" style="text-align: center;border:1px solid #000;width:200px;padding:40px;margin:auto">
-            <p>Please <a href="method.php" style="text-decoration:none">Sign In</a> To Continue</p>
-        </div>
+
+?>
 
 
     </body>
