9ff7dcdfceb8ec4941075c0c5026632a25e3d861
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2023-12-18 01:30:21 +0300

    create the place for dashboard settings

--- dashboard.php
+++ dashboard.php
@@ -17,15 +17,31 @@ if(isset($_SESSION['user_id']))
     </head>
     <body>
 
-<?php
-$htmlform1='<div id="signalert" style="text-align: center;border:1px solid #000;width:200px;padding:40px;margin:auto">
+
+
+<div id="signalert" style="display:none;text-align: center;border:1px solid #000;width:200px;padding:40px;margin:auto">
 <p>Please <a href="method.php" style="text-decoration:none">Sign In</a> To Continue</p>
-</div>';
-echo $signed ? "" : $htmlform1;
+</div>
 
 
-?>
+<div id="startwork" style="display: none;">
+great
+</div>
 
+<script>
+    
+function shows_me(rn)
+{
+if(rn==1)
+document.getElementById("signalert").style.display="block";
+else
+{
+    document.getElementById("startwork").style.display="block";
+}
+}
+shows_me(<?php echo $signed ? "0" : "1";?>);
+
+</script>
 
     </body>
 </html>
