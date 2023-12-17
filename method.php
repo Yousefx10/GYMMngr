01829b76c0a43e91f122ad8771b2721f552e3c9a
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2023-12-17 22:36:27 +0300

    login form

---
+++ method.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login To Continue</title>
+
+        <style>
+            form input{padding: 2px;margin: 20px;}
+            form label{display: inline-block;width: 200px;text-align: left;}
+            form input[type=submit]{font-size: 30px;padding: 20px;}
+        </style>
+    </head>
+    <body>
+
+        <div style="width: 500px;margin:auto;text-align:center;border:2px dotted #000">
+            <form>
+                <label>Enter Your LOGIN ID :</label>
+                <input type="text"/><br/>
+                <label>Enter Password :</label>
+                <input type="password"/><br/>
+                <input type="submit" value="Log In"/><br/>
+            </form>
+        </div>
+
+
+    </body>
+</html>
